b15f9c551ccbed07ae39598ac05622fc12fe06d7
Tam Bui <tam@roxova.com>
2020-03-22 22:40:24 +0700

    sign in with email and password

--- src/features/authentication/SigninForm.tsx
+++ src/features/authentication/SigninForm.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { useForm } from "react-hook-form"
 import FormInput from "../../components/FormInput"
 import CustomButton from "../../components/CustomButton"
-import { signInWithGoogle } from "../../config/firebase"
+import { signInWithGoogle, auth } from "../../config/firebase"
 
 type FormData = {
   email: string
@@ -10,9 +10,16 @@ type FormData = {
 }
 
 const SigninForm: React.FC = () => {
-  const { register, handleSubmit, errors, watch } = useForm<FormData>()
-  const onSubmit = (data: FormData) => console.log(data)
-
+  const { register, handleSubmit, errors, watch, reset } = useForm<FormData>()
+  const onSubmit = async (data: FormData) => {
+    const { email, password } = data
+    try {
+      await auth.signInWithEmailAndPassword(email, password)
+      reset()
+    } catch (error) {
+      console.error(error)
+    }
+  }
   return (
     <div className="mt-4">
       <h2 className="mb-4 text-3xl font-bold">I already have an account</h2>
